e710acb2cd072a39e842255011085c3b02ab9907
ajemraou <ajemraou@student.1337.ma>
2025-05-21 13:36:14 +0100

    configure MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
 
 const app = express();
+
+// Error handler middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
