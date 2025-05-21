92fa08d01e1f5f04bd0b92f8591c1a6d550548ce
ajemraou <ajemraou@student.1337.ma>
2025-05-21 15:01:08 +0100

    add routes for car and reservation APIs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const carRoutes = require("./routes/carRoutes");
+const reservationRoutes = require("./routes/reservationRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 // Load env vars
 dotenv.config();
@@ -9,6 +12,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+// Routes
+app.use("/api/cars", carRoutes);
+app.use("/api/reservations", reservationRoutes);
+app.use("/api/auth", authRoutes);
 
 // Error handler middleware
 app.use(errorHandler);
